a2029211aeac6766a6055928e7f9b82a185841a8
Abhinav dwivedi <learnerabhinavdwivedi@gmail.com>
2026-02-18 03:47:25 +0000

    logo

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def get_base64_image(image_path):
         return base64.b64encode(img_file.read()).decode()
 
 # Use your image file name here
-img_base64 = get_base64_image("Gemini_Generated_Image_6dai4k6dai4k6dai.png")
+img_base64 = get_base64_image("/workspaces/placementiq_pro2/Gemini_Generated_Image_6dai4k6dai4k6dai-removebg-preview.png")
 
 
 st.sidebar.markdown(
